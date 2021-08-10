c1dfb2fa15930786664d9b17e43b4d3d6f3a34b9
tsubakimoto <tkmbmbr@gmail.com>
2021-08-10 20:55:10 +0900

    add id property

--- cson/index.js
+++ cson/index.js
@@ -3,7 +3,8 @@ const CSON = require('cson');
 module.exports = async function (context, req) {
     context.log('Cson function processed a request.');
 
-    const csonObject = CSON.parse(req.body);
+    const csonString = `id: "${req.query.id}"\n${req.body}`;
+    const csonObject = CSON.parse(csonString);
     const jsonObject = CSON.createJSONString(csonObject, { replacer: undefined, indent: false});
 
     context.res = {
