37824776c705cc599208faa9b3cd6d6e5ebc82f1
tsubakimoto <tkmbmbr@gmail.com>
2021-08-12 00:17:38 +0900

    create mock page

---
+++ app/app.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    fetch('http://localhost:7071/api/notes')
+        .then(response => response.json())
+        .then(data => {
+            console.log('data', data);
+
+            let titles = '';
+            data.forEach(item => {
+                titles += `<li>${item.title}</li>`;
+            });
+
+            const app = document.getElementById('app');
+            app.innerHTML = `<ul>${titles}</ul>`;
+        })
+        .catch(error => console.log('error', error));
+};
