b279c0d09a1cc79ecdd5c2dfce04639998f72f8e
tsubakimoto <tkmbmbr@gmail.com>
2021-08-29 23:31:46 +0900

    Use Tailwind CSS

--- app/app.js
+++ app/app.js
@@ -1,16 +1,22 @@
+let notes;
+
 window.onload = function() {
     fetch('/api/notes')
         .then(response => response.json())
         .then(data => {
             console.log('data', data);
+            notes = data;
 
-            let titles = '';
+            let documents = '';
             data.forEach(item => {
-                titles += `<li>${item.title}</li>`;
+                const title = `<h5 class="mt-10 mb-3 font-semibold text-gray-900">${item.title}</h5>`;
+                const tags = item.tags.join(', ');
+                const content = `<p>Tags: ${tags}</p>`;
+                documents += `${title}${content}`;
             });
 
-            const app = document.getElementById('app');
-            app.innerHTML = `<ul>${titles}</ul>`;
+            const docs = document.getElementById('docs');
+            docs.innerHTML = documents;
         })
         .catch(error => console.log('error', error));
 };
