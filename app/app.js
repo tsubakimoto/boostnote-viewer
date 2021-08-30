1b96cc44c354d23f7480709b76fe7f0f6a78705a
tsubakimoto <tkmbmbr@gmail.com>
2021-08-30 22:17:44 +0900

    Update documents ui

--- app/app.js
+++ app/app.js
@@ -9,10 +9,10 @@ window.onload = function() {
 
             let documents = '';
             data.forEach(item => {
-                const title = `<h5 class="mt-10 mb-3 font-semibold text-gray-900">${item.title}</h5>`;
+                const title = `<h5 class="font-semibold text-gray-900">${item.title}</h5>`;
                 const tags = item.tags.join(', ');
                 const content = `<p>Tags: ${tags}</p>`;
-                documents += `${title}${content}`;
+                documents += `<div class="mt-5 mb-3 p-3 rounded-lg hover:bg-blue-50 cursor-pointer" id="doc_${item.id}">${title}${content}</div>`;
             });
 
             const docs = document.getElementById('docs');
