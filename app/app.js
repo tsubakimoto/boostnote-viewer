0df4403e7ccdac66a815b7b6c23eb5505bceba78
tsubakimoto <tkmbmbr@gmail.com>
2021-08-30 22:33:51 +0900

    show note content

--- app/app.js
+++ app/app.js
@@ -12,7 +12,7 @@ window.onload = function() {
                 const title = `<h5 class="font-semibold text-gray-900">${item.title}</h5>`;
                 const tags = item.tags.join(', ');
                 const content = `<p>Tags: ${tags}</p>`;
-                documents += `<div class="mt-5 mb-3 p-3 rounded-lg hover:bg-blue-50 cursor-pointer" id="doc_${item.id}">${title}${content}</div>`;
+                documents += `<div class="mt-5 mb-3 p-3 rounded-lg hover:bg-blue-50 cursor-pointer" onclick="showNote('${item.id}')">${title}${content}</div>`;
             });
 
             const docs = document.getElementById('docs');
@@ -20,3 +20,16 @@ window.onload = function() {
         })
         .catch(error => console.log('error', error));
 };
+
+function showNote(selectedId) {
+    const note = notes.find( ({ id }) => id === selectedId);
+    console.debug('note', note);
+
+    const contentElement = document.getElementById('content');
+    if (note === undefined || note === null) {
+        contentElement.innerHTML = '<p class="text-gray-600">Note is not found.</p>';
+    } else {
+        const content = note.snippets[0].content;
+        contentElement.innerHTML = content.replace(/\n/g, '<br>');
+    }
+}
